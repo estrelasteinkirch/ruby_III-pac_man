3efaac71831d481a479c144ad80745bebfd38d9d
Estrela <estrela.steinkirch@gmail.com>
2019-07-02 12:17:43 +0200

    adiciona mapas

---
+++ logic.rb
@@ -0,0 +1,22 @@
+require_relative 'ui'
+
+def le_mapa(numero)
+  arquivo = "mapa#{numero}.txt"
+  texto = File.read(arquivo)
+  mapa = texto.split("\n")
+end
+
+def joga(nome)
+   mapa =le_mapa(1)
+   while true
+    desenha(mapa)
+    direcao = pede_movimento
+
+  end
+
+end
+
+def inicia_pacman
+    nome = da_boas_vindas
+    joga(nome)
+end
