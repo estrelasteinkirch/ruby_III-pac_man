d25833358af41ecc0e5ee675b167f19c56944ec0
Estrela <estrela.steinkirch@gmail.com>
2019-07-03 09:38:18 +0200

    implementa movimentos do fantasma

--- logic.rb
+++ logic.rb
@@ -8,7 +8,7 @@ end
 
 def encontra_jogador(mapa)
   caracter_do_heroi = "H"
-  mapa.each_with_index do |linha_atual, linha|
+  mapa.each_with_index do |linha_atual, linha| #o texto da linha e a posição
       coluna_do_heroi = linha_atual.index(caracter_do_heroi) #index
       if coluna_do_heroi #dá verdadeiro se o valor nao for nil
         return [linha, coluna_do_heroi] 
@@ -17,6 +17,28 @@ def encontra_jogador(mapa)
   end
   # não achei!
 end
+
+def move_fantasma(mapa, linha, coluna)
+  posicao = [linha, coluna + 1]
+  if posicao_valida? mapa, posicao
+      mapa[linha][coluna] = " "
+      mapa[posicao[0]][posicao[1]] = "F"
+  end
+end
+
+def move_fantasmas(mapa)
+  caracter_do_fantasma = "F"
+  mapa.each_with_index do |linha_atual, linha|
+    linha_atual.chars.each_with_index do |caractere_atual, coluna| #o texto da coluna e a posição
+      #o chars transforma a string em um array de caracteres
+      eh_fantasma = caractere_atual == caracter_do_fantasma
+      if eh_fantasma
+        move_fantasma(mapa, linha, coluna)
+      end
+    end
+  end
+end
+
 
 def calcula_nova_posicao(heroi, direcao)
   heroi = heroi.dup
@@ -44,14 +66,15 @@ def posicao_valida?(mapa, posicao)
       return false
     end
 
-    if mapa[posicao[0]][posicao[1]] == "X"
+    valor_atual = mapa[posicao[0]][posicao[1]]
+    if valor_atual == "X" || valor_atual == "F"
       return false
     end
     true
 end
 
 def joga(nome)
-  mapa = le_mapa(1)
+  mapa = le_mapa(2)
   while true
     desenha mapa
     direcao = pede_movimento
@@ -63,6 +86,8 @@ def joga(nome)
     end
     mapa[heroi[0]][heroi[1]] = " "
     mapa[nova_posicao[0]][nova_posicao[1]] = "H"
+
+    move_fantasmas(mapa)
   end
 end
 
