085f8eb5295b46bdd0a80e4ecfb0f2443eac575e
Estrela <estrela.steinkirch@gmail.com>
2019-07-03 11:14:30 +0200

    erro

--- logic.rb
+++ logic.rb
@@ -18,12 +18,38 @@ def encontra_jogador(mapa)
   # não achei!
 end
 
-def move_fantasma(mapa, linha, coluna)
-  posicao = [linha, coluna + 1]
-  if posicao_valida? mapa, posicao
-      mapa[linha][coluna] = " "
-      mapa[posicao[0]][posicao[1]] = "F"
+def posicoes_validas_a_partir_de(mapa, posicao)
+  posicoes = []
+  baixo = mapa[posicao[0] + 1, posicao[1]]
+  if posicao_valida?(mapa, baixo)
+      posicoes << baixo
   end
+  direita = mapa[posicao[0], posicao[1] + 1]
+  if posicao_valida?(mapa, direita)
+      posicoes << direita
+  end
+  cima = mapa[posicao[0] - 1, posicao[1]]
+  if posicao_valida?(mapa, cima)
+      posicoes << cima
+  end
+  esquerda = mapa[posicao[0], posicao[1] - 1]
+  if posicao_valida?(mapa, esquerda)
+      posicoes << esquerda
+  end
+  posicoes 
+end
+
+def move_fantasma(mapa, linha, coluna)
+  posicoes = posicoes_validas_a_partir_de mapa, [linha, coluna]
+  
+  if posicoes.empty?
+    return #early return se nao tiver nenhuma posicao válida
+  end
+
+  posicao = posicoes[0]
+
+  mapa[linha][coluna] = " "
+  mapa[posicao[0]][posicao[1]] = "F"
 end
 
 def move_fantasmas(mapa)
@@ -95,3 +121,10 @@ def inicia_pacman
     nome = da_boas_vindas
     joga(nome)
 end
+
+
+
+
+
+
+
