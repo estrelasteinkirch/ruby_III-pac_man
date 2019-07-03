ba3d8a7376f93645f66df467f9427857a0c3bd94
Estrela <estrela.steinkirch@gmail.com>
2019-07-03 08:26:44 +0200

    Detecção de colisão com o muro e o fim do mapa

--- logic.rb
+++ logic.rb
@@ -6,14 +6,64 @@ def le_mapa(numero)
   mapa = texto.split("\n")
 end
 
-def joga(nome)
-   mapa =le_mapa(1)
-   while true
-    desenha(mapa)
-    direcao = pede_movimento
-
+def encontra_jogador(mapa)
+  caracter_do_heroi = "H"
+  mapa.each_with_index do |linha_atual, linha|
+      coluna_do_heroi = linha_atual.index(caracter_do_heroi) #index
+      if coluna_do_heroi #dá verdadeiro se o valor nao for nil
+        return [linha, coluna_do_heroi] 
+        # achei!
+      end
   end
+  # não achei!
+end
 
+def calcula_nova_posicao(heroi, direcao)
+  heroi = heroi.dup
+  case direcao
+    when "W"
+      heroi[0] -= 1
+    when "S"
+      heroi[0] += 1
+    when "A"
+      heroi[1] -= 1
+    when "D"
+      heroi[1] += 1
+  end
+  heroi
+end
+
+def posicao_valida?(mapa, posicao)
+    linhas = mapa.size
+    colunas = mapa[0].size
+
+    estourou_linha = posicao[0] < 0 || posicao[0] >= linhas
+    estourou_coluna = posicao[1] < 0 || posicao[1] >= colunas
+
+    if estourou_linha || estourou_coluna
+      return false
+    end
+
+    if mapa[posicao[0]][posicao[1]] == "X"
+      return false
+    end
+    true
+end
+
+def joga(nome)
+  mapa = le_mapa(1)
+  while true
+    desenha mapa
+    direcao = pede_movimento
+    heroi = encontra_jogador(mapa)
+      
+    nova_posicao = calcula_nova_posicao(heroi, direcao)
+    if !posicao_valida?(mapa, nova_posicao)
+      next
+    end
+    mapa[heroi[0]][heroi[1]] = " "
+    mapa[nova_posicao[0]][nova_posicao[1]] = "H"
+  end
 end
 
 def inicia_pacman
